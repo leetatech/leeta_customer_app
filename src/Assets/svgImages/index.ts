aca7b0b01956c9b63dfa9c2c244445e34fe39ef4
chiomaT <chiomanwabugwu11@yahoo.com>
2023-11-12 15:57:48 +0100

    LEET-167|feat:Image.

--- src/Assets/svgImages/index.ts
+++ src/Assets/svgImages/index.ts
@@ -1,6 +1,7 @@
 import USER from './user.svg'
 import LOADER from './loader.svg'
 import SUCCESSS from './success.svg'
+import WARNING_ICON from './exclaimation-circle.svg'
 
 
-export {USER,LOADER,SUCCESSS}
+export {USER,LOADER,SUCCESSS,WARNING_ICON}
