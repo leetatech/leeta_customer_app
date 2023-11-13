35aa568ffb019a43d967bd2a58cc8e28800e1f64
chiomaT <chiomanwabugwu11@yahoo.com>
2023-11-13 12:42:24 +0100

    LEET-169|feat:image

--- src/Assets/svgImages/index.ts
+++ src/Assets/svgImages/index.ts
@@ -5,5 +5,6 @@ import WARNING_ICON from './exclaimation-circle.svg'
 import OTP_IMAGE from './otp_icon.svg'
 import INFO_CIRCLE from './info-circle.svg'
 import CLOSE_INFO from './info_close.svg'
+import NAVIGATION_ICON from './back button.svg'
 
-export {USER,LOADER,SUCCESSS,WARNING_ICON,OTP_IMAGE,INFO_CIRCLE,CLOSE_INFO}
+export {USER,LOADER,SUCCESSS,WARNING_ICON,OTP_IMAGE,INFO_CIRCLE,CLOSE_INFO,NAVIGATION_ICON}
