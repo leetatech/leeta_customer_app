d359f68fdd9f36353a5b4111301eb161be305a1a
chiomaT <chiomanwabugwu11@yahoo.com>
2023-11-20 08:38:54 +0100

    LEET-174|feat:Changed onboarding images.

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -1,13 +1,14 @@
 const LOGO = require('./Images/logo.png');
-const HAPPY_GIRL2 = require('./Images/happy_girl2.png');
-const HAPPY_GIRL1 = require('./Images/happy_girl1.png');
+const SELLER = require('./Images/seller.png');
+const BUYER = require('./Images/buyer.png');
 const HAPPY_MAN = require('./Images/happy_man.png');
 const NAVIGATION_ARROW = require('./Images/back button.png')
 
+
 export {
   LOGO,
-  HAPPY_GIRL2,
-  HAPPY_GIRL1,
+  SELLER,
+  BUYER,
   HAPPY_MAN,
   NAVIGATION_ARROW
 };
