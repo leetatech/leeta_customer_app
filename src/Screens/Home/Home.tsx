05daebc01b1b777a49047b7051f3199913e28d63
chiomaT <chiomanwabugwu11@yahoo.com>
2024-01-26 14:21:51 +0100

    LEET-205|feat:Google map integration.

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,21 +1,11 @@
-import React, {FC, useMemo} from 'react';
-import {View, Text} from 'react-native';
+import React, {FC} from 'react';
 import {NavigationProp, ParamListBase} from '@react-navigation/native';
-import PaddedLayout from '../../Components/Layouts/PaddedLayout';
-import createStyles from './style';
+import GoogleMap from '../GoogleMap/GoogleMap';
 
 interface IProps {
   navigation: NavigationProp<ParamListBase>;
 }
 const Home: FC<IProps> = ({navigation}) => {
-  const styles = useMemo(() => createStyles(), []);
-  return (
-    <PaddedLayout>
-      <View style={styles.mainContainer}>
-        <Text>Home Screen</Text>
-      </View>
-    </PaddedLayout>
-  );
+  return <GoogleMap />;
 };
-
 export default Home;
