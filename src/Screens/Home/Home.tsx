ce8c4f9cb4735182938e2ea001d1110095cc0272
chiomaT <chiomanwabugwu11@yahoo.com>
2024-02-11 01:07:00 +0100

    LEET|feat:Created the Gass refill button, sample cylinder weights options and input field for custom cylynder weight input.

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,11 +1,128 @@
-import React, {FC} from 'react';
+import React, {FC,  useMemo, useRef, useState} from 'react';
 import {NavigationProp, ParamListBase} from '@react-navigation/native';
 import GoogleMap from '../GoogleMap/GoogleMap';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import createStyles from './style';
+import Buttons from '../../Components/Buttons/Buttons';
+import CustomModal from '../../Components/Modal/CustomModal';
+import {SvgIcon} from '../../Components/icons';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {CANCEL_ICON} from '../../Assets/svgImages';
+import {colors} from '../../Constants/Colors';
 
 interface IProps {
   navigation: NavigationProp<ParamListBase>;
 }
+
+const weightOptions = ['2 kg', '3 kg', '4 kg', '5 kg', '6 kg', '7 kg', 'Other'];
+
 const Home: FC<IProps> = ({navigation}) => {
-  return <GoogleMap />;
+  const styles = useMemo(() => createStyles(), []);
+  const [openGasWeightOptions, setopenGasWeightOptions] = useState(false);
+  const [inputWeight, setInputWeight] = useState(false);
+  const inputRef = useRef<TextInput>(null);
+
+  const focusInput = () => {
+    console.log('INPUTREF', inputRef);
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const handleOpenWeightOptions = () => {
+    setopenGasWeightOptions(true);
+  };
+  const handleCloseWeightOptions = () => {
+    setopenGasWeightOptions(false);
+    setInputWeight(false);
+  };
+  const handleInputWeight = (index: number) => {
+    if (index === weightOptions.length - 1) {
+      setInputWeight(true);
+    }
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <GoogleMap />
+      <View style={styles.button_container}>
+        <Buttons
+          title="Gas refill"
+          disabled={false}
+          buttonStyle={undefined}
+          textStyle={undefined}
+          onPress={handleOpenWeightOptions}
+        />
+      </View>
+      <ScrollView style={{flex: 1}}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{flex: 1}}>
+          <CustomModal visible={openGasWeightOptions}>
+            <View style={styles.modal_actions}>
+              <TouchableOpacity
+                onPress={() => handleInputWeight(weightOptions.length - 1)}>
+                <SvgIcon
+                  size={24}
+                  fill={inputWeight ? colors.WHITE : colors.GRAY}
+                  pathData="M10.75 4.75C10.75 4.33579 10.4142 4 10 4C9.58579 4 9.25 4.33579 9.25 4.75V9.25H4.75C4.33579 9.25 4 9.58579 4 10C4 10.4142 4.33579 10.75 4.75 10.75L9.25 10.75V15.25C9.25 15.6642 9.58579 16 10 16C10.4142 16 10.75 15.6642 10.75 15.25V10.75L15.25 10.75C15.6642 10.75 16 10.4142 16 10C16 9.58579 15.6642 9.25 15.25 9.25H10.75V4.75Z"
+                />
+              </TouchableOpacity>
+
+              <Text style={styles.text_color}>
+                {inputWeight ? 'Input Weight' : 'Select Weight'}
+              </Text>
+              <TouchableOpacity onPress={handleCloseWeightOptions}>
+                <CANCEL_ICON />
+              </TouchableOpacity>
+            </View>
+            {inputWeight ? (
+              <View style={styles.input_weight_container}>
+                <View>
+                  <TouchableOpacity
+                    style={styles.input_weight_and_unit_container}
+                    onPress={focusInput}>
+                    <TextInput
+                      placeholderTextColor={colors.LGRAY}
+                      ref={inputRef}
+                      style={styles.input_style}
+                    />
+                    <Text style={styles.unit}>Kg</Text>
+                  </TouchableOpacity>
+                </View>
+                <Buttons
+                  title="Continue"
+                  disabled={false}
+                  buttonStyle={{paddingBottom: 5}}
+                  textStyle={undefined}
+                />
+              </View>
+            ) : (
+              <View>
+                <View style={styles.weight_options_main_container}>
+                  {weightOptions.map((kg, index) => (
+                    <View key={index} style={styles.weight_options_container}>
+                      <TouchableOpacity
+                        onPress={() => handleInputWeight(index)}>
+                        <Text style={styles.text_color}>{kg}</Text>
+                      </TouchableOpacity>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            )}
+          </CustomModal>
+          <View style={{height: 30}} />
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  );
 };
 export default Home;
--- src/Screens/Home/style.ts
+++ src/Screens/Home/style.ts
@@ -1,13 +1,63 @@
 import {StyleSheet} from 'react-native';
+import {colors} from '../../Constants/Colors';
 
 const createStyles = () =>
   StyleSheet.create({
-    mainContainer:{
-     justifyContent: 'center',
-     alignItems: 'center',
+    mainContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
     },
- 
-   
+    button_container: {
+      position: 'absolute',
+      bottom:0
+    },
+    modal_actions: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      borderBottomWidth: 1,
+      borderBottomColor: colors.XLGRAY,
+      paddingVertical: 10,
+    },
+    weight_options_container: {
+      paddingVertical: 13,
+      fontSize: 18,
+      borderBottomColor: colors.XLGRAY,
+      borderBottomWidth: 1,
+      justifyContent: 'center',
+      paddingTop: 10,
+      flexGrow: 1,
+    },
+    weight_options_main_container: {
+      height: 300,
+      marginBottom: 15,
+    },
+    text_color: {
+      color: colors.GRAY,
+      fontWeight: '600',
+      fontSize: 15,
+    },
+    input_weight_container: {
+      gap: 10,
+    },
+    input_weight_and_unit_container: {
+      flexDirection: 'row',
+      borderWidth: 1,
+      borderColor: colors.LGRAY,
+      paddingHorizontal: 10,
+      paddingVertical: 15,
+      borderRadius: 5,
+      alignContent: 'center',
+    },
+    input_style: {
+      fontWeight: '700',
+      fontSize: 20,
+      letterSpacing: 2,
+    },
+    unit: {
+      paddingTop: 8,
+      fontWeight: '600',
+    },
   });
 
 export default createStyles;
