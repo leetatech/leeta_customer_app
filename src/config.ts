13627ca1124f4ba4404a49e8a8221052d47424c0
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-05-23 13:00:50 +0100

    LEET-2 | Endpoint modfication

--- src/config.ts
+++ src/config.ts
@@ -15,8 +15,8 @@ export const apiUrl: ApiUrls = {
   signUp: `${BASE_URL}session/signup`, // POST
   otpVerification: `${BASE_URL}session/otp/validate`, // POST
   logIn: `${BASE_URL}session/signin`, // POST
-  forgotPassword: `${BASE_URL}session/password/forgot`, // POST
-  passwordReset: `${BASE_URL}session/password/create`, // POST
+  forgotPassword: `${BASE_URL}session/password/forgot` , // POST
+  passwordReset: `${BASE_URL}session/password/create` , // POST
   resendOtp: `${BASE_URL}session/otp/request`, //POST
   productList:`${BASE_URL}product`, // PUT
   feesType:`${BASE_URL}fees` // PUT
