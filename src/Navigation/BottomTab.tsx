0634e8adfa4bbff3b725f53a3121e8169e97c153
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-10-01 08:12:56 +0100

    LEET-280

--- src/Navigation/BottomTab.tsx
+++ src/Navigation/BottomTab.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, Settings} from '../Screens';
+import {Home, Orders, Settings} from '../Screens';
 import {colors} from '../Constants/Colors';
 import {SvgIcon} from '../Components/icons';
 
@@ -34,6 +34,19 @@ const BottomNavigator = () => {
       <Tab.Screen
         name="Home"
         component={HomeStackScreens}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <SvgIcon
+              size={24}
+              fill={focused ? colors.ORANGE : colors.LGRAY}
+              pathData="M10.6798 18.6189V13.2856H14.9533V18.6189H20.2952V11.5078H23.5003L12.8166 3.50781L2.13281 11.5078H5.33794V18.6189H10.6798Z"
+            />
+          ),
+        }}
+      />
+        <Tab.Screen
+        name="Orders"
+        component={Orders}
         options={{
           tabBarIcon: ({focused}) => (
             <SvgIcon
