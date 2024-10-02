eaf607b10837ed034454dbacee4bebed8d374796
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2024-10-02 14:18:16 +0100

    LEET-280|Order screeen

--- src/Screens/Orders/styles.ts
+++ src/Screens/Orders/styles.ts
@@ -3,12 +3,35 @@ import {colors} from '../../Constants/Colors';
 
 const createStyles = () =>
   StyleSheet.create({
-    container: {
+    main_container: {
+      backgroundColor: colors.WHITE,
       flex: 1,
-      alignItems: 'center',
-      paddingTop: 20,
+    },
+    container: {
       backgroundColor: colors.WHITE,
       paddingHorizontal: 15,
+    },
+
+    order_description_container: {
+      flexDirection: 'row',
+      gap: 20,
+      paddingTop: 20,
+      borderBottomColor: colors.XLGRAY,
+      borderBottomWidth: 2,
+    },
+    order_container: {
+      paddingVertical: 20,
+      marginBottom: 20,
+    },
+
+    activeContainer: {
+      borderColor: colors.ORANGE,
+      borderBottomWidth: 2,
+    },
+    inactiveContainer: {
+      borderColor: colors.TRANSPARENT,
+
+      borderBottomWidth: 0,
     },
   });
 
---
+++ src/Screens/Orders/types.ts
@@ -0,0 +1,5 @@
+export interface ComponentType {
+    openOrders: boolean;
+    deliveredOrders: boolean;
+    cancelledOrders: boolean;
+  }
